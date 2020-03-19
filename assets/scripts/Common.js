baec7423f677a15c87370e9a5dbd0d6a528dd114
legianha592 <51075654+legianha592@users.noreply.github.com>
2020-03-19 14:30:16 +0700

    add prefab

--- assets/scripts/Common.js
+++ assets/scripts/Common.js
@@ -3,17 +3,17 @@ var Common = {
     showPopup(name_popup, callback){
         var scene = cc.director.getScene();
         if (cc.isValid(scene) && !cc.isValid(scene.getChildByName(name_popup))){
-            cc.loader.loadRes("prefabs/", function(err, popup){
+            cc.loader.loadRes("prefabs/" + name_popup, function(err, popup){
                 if (!err){
                     var popupItem = cc.instantiate(popup);
                     if (cc.isValid(popupItem) && cc.isValid(scene)){
-                        popupItem.x = cc.director.getScene().width/2;
-                        popupItem.y = cc.director.getScene().height/2;
+                        popupItem.x = cc.director.getWinSize().width/2;
+                        popupItem.y = cc.director.getWinSize().height/2;
                         if (callback){
                             var popupComp = popupItem.getComponent(name_popup);
                             popupComp.setNamePopup(name_popup);
                             callback(popupComp);
-                            scene.addChild(popup, 1000)
+                            scene.addChild(popupItem, 1000)
                         }
                     }
                 }
